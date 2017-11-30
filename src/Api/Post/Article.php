57cc29c43772e5a33627f77faabc8d140aec42f9
Adam Balan <AdamKyleBalan@gmail.com>
2017-11-29 17:32:11 -0700

    Added a new Post API - this is for development only.

---
+++ src/Api/Post/Article.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace HelpscoutApi\Post;
+
+use GuzzleHttp\Client;
+use HelpscoutApi\Contracts\ArticlePostBody;
+
+/**
+ * Allows you to post an article to Helpscout using the API.
+ *
+ * @link https://developer.helpscout.com/docs-api/articles/create/
+ */
+class Article {
+
+  /**
+   * GuzzleHttp\Client
+   */
+  private $client;
+
+  /**
+   * HelpscoutApi\Contracts\ApiKey;
+   */
+  private $apiKey;
+
+  public function __construct(Client $client, ApiKey $apiKey) {
+      $this->client = $client;
+      $this->apiKey = $apiKey->getKey();
+  }
+
+  /**
+   * Post an article to the Helpscout API.
+   *
+   * @param ArticlePostBody
+   * @link https://developer.helpscout.com/docs-api/articles/create/
+   */
+  public function create(ArticlePostBody $articlePostBody) {
+    $response = $this->client->request(
+      'POST',
+      'articles',
+      [
+          'headers' => [
+              'Accept' => 'application/json',
+              'Content-Type' => 'application/json',
+           ],
+          'auth' => [$this->apiKey, 'X'],
+          'body' => $articlePostBody->createPostBody(),
+      ]
+    );
+
+    return $response;
+  }
+
+}
---
+++ src/Contracts/ArticlePostBody.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace HelpscoutApi\Contracts;
+
+/**
+ * An Article interface for creating only the required fields.
+ *
+ * @link https://developer.helpscout.com/docs-api/articles/create/
+ */
+interface ArticlePostBody {
+
+  /**
+   * Set the collection ID the aerticle belongs to.
+   *
+   * @param String
+   */
+  public function collectionID(String $collectionId);
+
+  /**
+   * Set the name of the aerticle.
+   *
+   * @param String
+   */
+  public function name(String $name);
+
+  /**
+   * Set the text of the aerticle.
+   *
+   * @param String
+   */
+  public function text(String $text);
+
+  /**
+   * Create the JSON aspect of the post body.
+   *
+   * ```php
+   * {
+   *   collectionId: 'xxxx',
+   *   name: 'sample',
+   *   text: 'Some text here ...',
+   * }
+   * ````
+   *
+   * @return JSON
+   */
+  public function createPostBody();
+}
